849dd0d0a59e464d0bb3e8b96204f6eb79410fb7
Alexey Arbuzov <91293alex@gmail.com>
2017-11-20 21:02:44 +0300

    add ability to add records

--- src/components/RecordsEntity/index.js
+++ src/components/RecordsEntity/index.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react'
+import { Button } from 'reactstrap';
 import './index.css'
 
 export class RecordEntity extends Component {
+  handleDeleteRecord = () => {
+    const { id, onDelete } = this.props
+    onDelete(id)
+  }
+
   render () {
     const { recordEntity } = this.props
-    const { name, description, initialPrice, price, priceDifference } = recordEntity
+    const { name, description, initialPrice, price } = recordEntity
     return (
       <tr className='juice-tracker-RecordsEntity-item'>
         <td>{name}</td>
         <td>{description}</td>
         <td>{initialPrice}</td>
         <td>{price}</td>
-        <td>{priceDifference}</td>
+        <td>
+          <Button
+            className='juice-tracker-RecordsEntity-btn'
+            onClick={this.handleDeleteRecord}
+          >x
+          </Button>
+        </td>
       </tr>
     )
   }
