d73aafd337c52a95e0e2fb50fca291fe4ec43fc0
Alexey Arbuzov <91293alex@gmail.com>
2017-11-20 18:59:45 +0300

    add record entity

--- src/redux/reducers/records.js
+++ src/redux/reducers/records.js
@@ -1,5 +1,38 @@
 
-const initialState = {}
+const initialState = [
+  {
+    id: 1,
+    name: 'Angry Vape',
+    description: 'Diego bull',
+    initialPrice: 200,
+    price: 280,
+    priceDifference: 80
+  },
+  {
+    id: 2,
+    name: 'Angry Vape',
+    description: 'David monkey',
+    initialPrice: 200,
+    price: 280,
+    priceDifference: 80
+  },
+  {
+    id: 3,
+    name: 'Jam monster',
+    description: 'Grape',
+    initialPrice: 1000,
+    price: 1300,
+    priceDifference: 300
+  },
+  {
+    id: 4,
+    name: 'Jam monster',
+    description: 'Apple',
+    initialPrice: 1000,
+    price: 1300,
+    priceDifference: 300
+  }
+]
 
 export default function records (state = initialState, action) {
   switch (action.type) {
